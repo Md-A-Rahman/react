72a0b19917e45d084bc922e12878c93806a996ad
Mohammed Abdul Rahman <160421733304@mjcollege.ac.in>
2025-08-29 01:10:04 +0530

    testing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+            <p>This is the footer</p>
+        </footer>
+    </div>
+  )
+}
+
+
+export default Footer
--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,7 +1,7 @@
 const Greet = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Hello bro, i am testing</h1>
         </div>
     )
 }
